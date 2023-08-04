6e1b70217599986380924c7591d8e22d3b43392d
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-08-04 12:19:30 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
@@ -1,15 +1,7 @@
 class Solution {
     fun solution(x: Int): Boolean {
-
-        var a = x.toString()
-
-        var b = a.map { it.toString().toInt()}
-
-        var c = b.sum()
-        
-        var d = x % c
-
-        if(d == 0) return true
+        val c = x.toString().sumOf { it - '0' }
+        if(x % c == 0) return true
         
         return false
     }
