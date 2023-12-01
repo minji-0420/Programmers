3fb2ff3a00f6084254a96d901aee261ca8fe6d16
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-01 15:49:58 +0900

    [level unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(ineq: String, eq: String, n: Int, m: Int): Int {
+       return when {
+        ineq == ">" && eq == "=" -> if (n >= m) 1 else 0
+        ineq == "<" && eq == "=" -> if (n <= m) 1 else 0
+        ineq == ">" && eq == "!" -> if (n > m) 1 else 0
+        ineq == "<" && eq == "!" -> if (n < m) 1 else 0
+        else -> throw IllegalArgumentException("Invalid input")
+    }
+}
+}
