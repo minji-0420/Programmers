266941e7eda322e64ae70f422ddf4b22e2acb46e
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-06 22:48:32 +0900

    [level unrated] Title: 접미사 배열, Time: 18.63 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.kt
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(my_string: String) : ArrayList<String>{
-        var answer = ArrayList<String>()
-        val stringBuilder = StringBuilder()
-        for (i in my_string.indices) {
-            answer.add(my_string.substring(i, my_string.length))
-        }
-        answer.sort()
-        answer.forEach { stringBuilder.append(it).append("\n") }
-        return answer
-    }
+    fun solution(my_string: String) = my_string.indices.map(my_string::substring).sorted()
 }
