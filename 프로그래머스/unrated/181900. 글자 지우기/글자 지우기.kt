3fde041a61923647a9767f50c1678c79a57d602f
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-08 11:55:41 +0900

    [level unrated] Title: 글자 지우기, Time: 14.37 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.kt
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.kt
@@ -1,14 +1,3 @@
 class Solution {
-    fun solution(my_string: String, indices: IntArray): String {
-        val sortedIndices = indices.sortedDescending().toIntArray()
-        val result = StringBuilder(my_string)
-
-        for (index in sortedIndices) {
-            if (index < result.length) {
-                result.deleteCharAt(index)
-            }
-        }
-
-        return result.toString()
-    }
+    fun solution(my_string: String, indices: IntArray) = my_string.filterIndexed { index, _ ->  index !in indices}
 }
