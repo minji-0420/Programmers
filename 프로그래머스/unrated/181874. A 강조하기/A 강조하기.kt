7f83b792c799af9f0a510353305fdd533192600a
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-13 09:35:39 +0900

    [level unrated] Title: A 강조하기, Time: 7.78 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
@@ -1,15 +1,3 @@
 class Solution {
-    fun solution(myString: String): String {
-        var answer: String = ""
-        for (i in myString.indices) {
-            if (myString[i] == 'a') {
-                answer += myString[i].uppercase()
-            } else if (myString[i] == 'A') {
-                answer += 'A'
-            } else {
-                answer += myString[i].lowercase()
-            }
-        }
-        return answer
-    }
+    fun solution(myString: String) = myString.lowercase().replace('a', 'A')
 }
--- 프로그래머스/unrated/181874. A 강조하기/README.md
+++ 프로그래머스/unrated/181874. A 강조하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] A 강조하기 - 181874 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874) 
 
 ### 성능 요약
 
-메모리: 61.9 MB, 시간: 2.49 ms
+메모리: 60.9 MB, 시간: 7.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 9:34:32
+2023년 12월 3일 9:35:41
 
 ### 문제 설명
 
