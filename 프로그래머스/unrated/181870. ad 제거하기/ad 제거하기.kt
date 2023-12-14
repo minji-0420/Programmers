f39077a6876dec1d244209756e3942b9e513bb42
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-14 09:54:38 +0900

    [level unrated] Title: ad 제거하기, Time: 9.98 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.kt
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(strArr: Array<String>): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-        for (str in strArr) {
-            if (!str.contains("ad")) {
-                answer += str
-            }
-        }
-        return answer
-    }
+    fun solution(strArr: Array<String>) = strArr.filter { !it.contains("ad") }
 }
