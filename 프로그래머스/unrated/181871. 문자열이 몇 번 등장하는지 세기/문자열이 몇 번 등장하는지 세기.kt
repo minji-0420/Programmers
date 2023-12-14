b0da3fe8d7c229ab4475534b4ae6456f2855e3b8
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-14 09:36:52 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 6.97 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(myString: String, pat: String): Int {
+        var count = 0
+        var index = myString.indexOf(pat)
+        while (index != -1) {
+            count++
+            index = myString.indexOf(pat, index + 1)
+        }
+        return count
+    }
+}
