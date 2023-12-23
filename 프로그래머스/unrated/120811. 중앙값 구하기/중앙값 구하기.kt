f0d43c1befebb3bcaf0cf818f2113064b4571046
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-23 12:21:12 +0900

    [level unrated] Title: 중앙값 구하기, Time: 13.63 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.kt
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(array: IntArray): Int {
-        var answer: Int = 0
-        val sortedArray = array.sorted()
-        if (sortedArray.size % 2 != 0) {
-            answer = sortedArray[sortedArray.size / 2]
-        }
-        return answer
-    }
+    fun solution(array: IntArray) = array.sorted()[array.size / 2]
 }
