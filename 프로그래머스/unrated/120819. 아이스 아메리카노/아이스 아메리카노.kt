284c6a813f1f1f857add286a74605f1d5fd22885
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-25 07:54:03 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.kt
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(money: Int): IntArray {
-        var price = 5500
-        var max = money / price
-        var rest = money % price
-
-        return intArrayOf(max, rest)
-    }
-       
+    fun solution(money: Int) = intArrayOf(money / 5500, money % 5500)   
 }
