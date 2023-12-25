677eacdeb630666db003165b6044e906599839c4
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-26 08:36:54 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,8 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        answer += num_list.count { it % 2 == 0 }
-        answer += num_list.count { it % 2 == 1 }
-        return answer
-    }
+    fun solution(num_list: IntArray) = intArrayOf(num_list.count { it % 2 == 0 }, num_list.count { it % 2 == 1 })
 }
