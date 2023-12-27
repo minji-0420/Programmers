f218ca0ebe3c487f21826600cdeb8acb22743e69
Minji Kim <139092737+minji-0420@users.noreply.github.com>
2023-12-27 09:52:06 +0900

    [level unrated] Title: 양꼬치, Time: 0.01 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.kt
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.kt
@@ -1,10 +1,4 @@
 class Solution {
-    fun solution(n: Int, k: Int): Int {
-        var answer: Int = 0
-        answer = n * 12000 + k * 2000
-        if (n >= 10) {
-            answer = n * 12000 + k * 2000 - (n / 10) * 2000
-        }
-        return answer
-    }
+    fun solution(n: Int, k: Int) = n * 12000 + k * 2000 - (n / 10) * 2000
+     
 }
